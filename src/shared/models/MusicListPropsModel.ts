5723dd03c8f89cc215297008dd53cefca3e7dbf7
Strayker <45916090+StraykerPL@users.noreply.github.com>
2024-06-25 22:38:59 +0200

    Add Flutter version of MusicApp

--- src/shared/models/MusicListPropsModel.ts
+++
@@ -1,7 +0,0 @@
-import { Asset } from "expo-media-library";
-import { Dispatch, SetStateAction } from "react";
-
-export interface MusicListPropsModel {
-    list: Asset[],
-    updateItem: Dispatch<SetStateAction<string | undefined>>
-}
